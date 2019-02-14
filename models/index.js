805a455459102d47e72e27c9f299d29a8aae877b
Egor <zangedzu333gig@gmail.com>
2019-02-14 20:23:15 +0300

    task01

---
+++ models/index.js
@@ -0,0 +1,35 @@
+module.exports = (Sequelize, config) => {
+    const options = {
+        host: config.host,
+        dialect: 'mysql',
+        logging: false,
+        define: {
+            timestamps: true,
+            paranoid: true,
+            defaultScope: {
+                where: { deletedAt: { $eq: null }}
+            }
+        }
+    }
+
+    const sequelize = new Sequelize('cwp21', 'root', '11111', options);
+
+    const Agents = require('../models/agents')(Sequelize, sequelize);
+    const Offices = require('../models/offices')(Sequelize, sequelize);
+    const Property = require('../models/properties')(Sequelize, sequelize);
+
+    Agents.hasMany(Property, {foreignKey: 'agentId'});
+    Property.belongsTo(Agents, {constraints: false, foreignKey: 'agentId'});
+
+    Offices.hasMany(Agents, {foreignKey: 'officeId'});
+    Agents.belongsTo(Offices, {constraints: false, foreignKey: 'officeId'});
+
+    return {
+        agents: Agents,
+        offices: Offices,
+        properties: Property,
+
+        sequelize,
+        Sequelize,
+    };
+};
---
+++ models/properties.js
@@ -0,0 +1,13 @@
+module.exports = (Sequelize, sequelize) => {
+    return sequelize.define('properties', {
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        heading: Sequelize.STRING,
+        price: Sequelize.INTEGER,
+        currency: Sequelize.STRING,
+        location: Sequelize.STRING
+    });
+};
